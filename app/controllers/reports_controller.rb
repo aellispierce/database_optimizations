4348e7a027010f41fb2fdf152922b5786a7c7d05
Ashley Ellis <anellis12@gmail.com>
2015-03-11 15:41:16 -0400

    Move file creation to delayed job

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,14 +5,14 @@ class ReportsController < ApplicationController
 
   def send_email
      SendEmailJob.perform_later(params[:email], params[:assembly][:name])
-     save_record(params[:assembly][:name])
+     # save_record(params[:assembly][:name])
      redirect_to root_path, notice: "Email sent!"
   end
 
-  def save_record(record_name)
-  	assembly= Assembly.find_by_name(record_name)
-    File.open("#{Rails.root}/tmp/page- #{rand(1000000000)}", "w") do |file|
-    	file << render_to_string(template: "report_mailer/report", layout: "mailer", :locals => {:@assembly => assembly, :@hits => assembly.hits})
-    end
-  end
+  # def save_record(record_name)
+  # 	assembly= Assembly.find_by_name(record_name)
+  #   File.open("#{Rails.root}/tmp/page- #{rand(1000000000)}", "w") do |file|
+  #   	file << render_to_string(template: "report_mailer/report", layout: "mailer", :locals => {:@assembly => assembly, :@hits => assembly.hits})
+  #   end
+  # end
 end
--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -3,6 +3,8 @@ class ReportMailer < ApplicationMailer
     @address = address
     @assembly = Assembly.find_by_name(name)
     @hits = @assembly.hits.order("percent_similarity DESC")
+    File.open("#{Rails.root}/tmp/page- #{rand(1000000000)}", "w") do |file|
+    	file << render_to_string(template: "report_mailer/report", layout: "mailer")
     mail(to: @address, subject: "Here is your report")
   end
 end
